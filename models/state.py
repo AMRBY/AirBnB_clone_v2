cf46ca7521fc1660f6b159d04b5e5566c5bca730
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-03-24 21:06:24 +0000

    mandatory

--- models/state.py
+++ models/state.py
@@ -1,9 +1,30 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
-
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+from models.city import City
+from os import getenv
 
 class State(BaseModel, Base):
     """ State class """
     __statename__ = 'states'
-    name = Column(String(128), nullable=False)
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', cascade='all, delete', backref='state')
+    else:
+        name = ''
+
+        @property
+        def cities(self):
+            """ Getter method
+            Returns a list of City instances with state_id == State.id
+            """
+            from models import storage
+
+            city_list = []
+            for val in storage.all(City).values():
+                if val.state_id == self.id:
+                    city_list.append(val)
+
+            return city_list
