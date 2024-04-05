b727c8ed211ab222fe16c4049d48ae30c5db4658
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-04-05 17:38:32 +0000

    mandatory

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """ this is a fab script to archive a directory"""
-from fabric.api import task, local
+from fabric.api import local
 from datetime import datetime
 
-@task
 def do_pack():
     """ create a directory and archive"""
-    now = datetime.now.strftime("%Y%m%d%H%M%S")
+    now = datetime.now()
+    now_f = now.strftime("%Y%m%d%H%M%S")
     local('mkdir -p versions')
-    result = local(f'tar -caf versions/web_static_{now}.tgz web_static')
+    result = local(f'tar -caf versions/web_static_{now_f}.tgz web_static')
     print(result.failed)
