4088f3b88d99141ca758dfa9422aea6175706600
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-04-05 03:10:03 +0000

    mandatory

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -11,6 +11,6 @@ fi
 ln -s /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data
 red_from="location / {"
-red_to="location /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
+red_to="location /hbnb_static/ {\n\t\talias /data/web_static/current/;\n"
 sed -i "s#$red_from#$red_to#" /etc/nginx/sites-available/default
 service nginx restart
