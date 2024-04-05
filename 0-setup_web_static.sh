f54c2d364a9e898f1cc6cd3848f5f17c649bdc42
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-04-05 02:20:17 +0000

    mandatory

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,26 +4,8 @@
 if (! dpkg-query -W "nginx" >/dev/null 2>&1); then
 	sudo apt-get -y install nginx
 fi
-if [ ! -d ./data ]; then
-	mkdir ./data/
-	mkdir ./data/web_static/
-	mkdir ./data/web_static/releases/
-	mkdir ./data/web_static/shared/
-	mkdir ./data/web_static/releases/test/
-elif [ ! -d ./data/web_static ]; then
-	mkdir ./data/web_static/
-	mkdir ./data/web_static/releases/
-	mkdir ./data/web_static/shared/
-	mkdir ./data/web_static/releases/test/
-elif [ ! -d ./data/web_static/releases ]; then
-	mkdir ./data/web_static/releases/
-	mkdir ./data/web_static/releases/test/
-elif [ ! -d ./data/web_static/shared ]; then
-	mkdir ./data/web_static/shared/
-elif [ ! -d ./data/web_static/releases/test ]; then
-	mkdir ./data/web_static/releases/test/
-fi
-
+mkdir -p /data/web_static/releases/test
+mkdir -p /data/web_static/shared
 echo -e "<html>
 	<head>
 	</head>
