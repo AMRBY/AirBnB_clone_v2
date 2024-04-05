ac5c43492754e4fd4f2f73e99fa0fb8e1d84b7ff
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-04-05 03:05:05 +0000

    mandatory

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,7 +5,10 @@ apt-get -y install nginx
 mkdir -p /data/web_static/releases/test
 mkdir -p /data/web_static/shared
 echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+if [ -L /data/web_static/current ]; then
+	rm /data/web_static/current
+fi
+ln -s /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data
 red_from="location / {"
 red_to="location /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
