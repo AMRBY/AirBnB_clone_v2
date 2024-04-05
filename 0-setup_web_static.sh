da1757c9a617e78772fa06538c00cfa1e7a4ed4a
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-04-05 02:48:55 +0000

    mandatory

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
-# Sets up a web server for the deployment of web_static
+# create web static for a server
 
-apt-get update
-apt-get install -y nginx
-
+apt-get -y install nginx
 mkdir -p /data/web_static/releases/test
 mkdir -p /data/web_static/shared
-echo "Hello World!" > /data/web_static/releases/test/index.html
-ls -sf /data/web_static/releases/test/ /data/web_static/current
-
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
-
-sed -i "61i\ \n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t\tautoindex off;\n\t}" /etc/nginx/sites-available/default
-
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data
+red_from="location / {"
+red_to="location /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
+sed -i "s#$red_from#$red_to#" /etc/nginx/sites-available/default
 service nginx restart
