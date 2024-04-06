ecfaca31dd7a2ccc34053d5144cadd53800ae9b2
AMR BY <bouyoussefamr@gmail.com>
2024-04-06 16:51:37 +0000

    mandatory

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,13 +1,26 @@
 #!/usr/bin/python3
 """ this is a fab script to archive a directory"""
-from fabric.api import local
+from fabric.api import env
+from fabric.operations import run, put, local
 from datetime import datetime
 
 
-def do_pack():
+env.hosts = ['35.175.129.122', '34.224.16.6']
+
+
+def do_deploy(archive_path):
     """ create a directory and archive"""
-    now = datetime.now()
-    now_f = now.strftime("%Y%m%d%H%M%S")
-    local('mkdir -p versions')
-    result = local(f'tar -caf versions/web_static_{now_f}.tgz web_static')
-    print(result.failed)
+    res = local(f'ls {archive_path}')
+    if res.failed:
+        return False
+    name_tar = local(f'echo {archive_path} | cut -d/ -f2', capture=True)
+    name = local(f'echo {name_tar} |cut -d. -f1', capture=True)
+    put(f'{archive_path}', "/tmp/")
+    run(f'mkdir -p /data/web_static/releases/{name}')
+    run(f'tar -xzf /tmp/{name_tar} -C /data/web_static/releases/{name}')
+    run(f'rm /tmp/{name_tar}')
+    run(f'mv /data/web_static/releases/{name}/web_static/*\
+            /data/web_static/releases/{name}')
+    run(f'rm -rf /data/web_static/releases/{name}/web_static')
+    run(f'rm -rf /data/web_static/current')
+    run(f'ln -s /data/web_static/releases/{name} /data/web_static/current')
