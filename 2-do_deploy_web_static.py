f79114e4a0facb55bb4ffdb199734b09d35fec9f
AMR BY <bouyoussefamr@gmail.com>
2024-04-06 22:09:47 +0000

    mandatory

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,8 +2,7 @@
 """ this is a fab script to archive a directory"""
 from fabric.api import env
 from fabric.operations import run, put, local
-from datetime import datetime
-
+from os.path import exists
 
 env.hosts = ['35.175.129.122', '34.224.16.6']
 
@@ -11,23 +10,25 @@ env.hosts = ['35.175.129.122', '34.224.16.6']
 def do_deploy(archive_path):
     """ create a directory and archive"""
 
-    try:
-        res = local(f'ls {archive_path}')
-        if res.failed:
+    if exists(archive_path):
+        try:
+            name_tar = local(f'echo {archive_path} \
+                    | cut -d/ -f2', capture=True)
+            name = local(f'echo {name_tar} |cut -d. -f1', capture=True)
+            put(f'{archive_path}', "/tmp/")
+            run(f'mkdir -p /data/web_static/releases/{name}')
+            run(f'tar -xzf /tmp/{name_tar} -C\
+                    /data/web_static/releases/{name}')
+            run(f'rm /tmp/{name_tar}')
+            run(f'mv /data/web_static/releases/{name}/web_static/*\
+                    /data/web_static/releases/{name}')
+            run(f'rm -rf /data/web_static/releases/{name}/web_static')
+            run(f'rm -rf /data/web_static/current')
+            run(f'ln -s /data/web_static/releases/{name} \
+                    /data/web_static/current')
+            print("New version deployed!")
+            return True
+        except Exception:
             return False
-        name_tar = local(f'echo {archive_path} | cut -d/ -f2', capture=True)
-        name = local(f'echo {name_tar} |cut -d. -f1', capture=True)
-        put(f'{archive_path}', "/tmp/")
-        run(f'mkdir -p /data/web_static/releases/{name}')
-        run(f'tar -xzf /tmp/{name_tar} -C /data/web_static/releases/{name}')
-        run(f'rm /tmp/{name_tar}')
-        run(f'mv /data/web_static/releases/{name}/web_static/*\
-                /data/web_static/releases/{name}')
-        run(f'rm -rf /data/web_static/releases/{name}/web_static')
-        run(f'rm -rf /data/web_static/current')
-        run(f'ln -s /data/web_static/releases/{name} /data/web_static/current')
-        print("New version deployed!")
-        return True
-
-    except Exception:
+    else:
         return False
