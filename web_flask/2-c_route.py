8c6cda31fb715fae5d9e3a51265940f304ca50ed
AMR BY <bouyoussefamr@gmail.com>
2024-04-20 20:32:57 +0000

    mandatory

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" flask framework """
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """ it displays Hello"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ it displays Hello"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """ it displays Hello"""
+    text = text.replace('_', ' ')
+    return f'C {escape(text)}'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
