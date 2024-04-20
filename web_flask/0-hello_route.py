de656f09c89be15a07b023ea96f95fddfab46626
AMR BY <bouyoussefamr@gmail.com>
2024-04-20 16:11:33 +0000

    mandatory

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-
+""" flask framework """
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
+    """ it displays Hello"""
     return "Hello HBNB!"
 
+
 if __name__ == '__main__':
-   app.run(debug=True) 
+    app.run()
