66bb0cccad1468fcae6c12e6bdf6cd7c93dc40f2
AMR BY <bouyoussefamr@gmail.com>
2024-04-20 21:01:13 +0000

    mandatory

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" flask framework """
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """ it displays Hello"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ it displays Hello"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """ it displays Hello"""
+    text = text.replace('_', ' ')
+    return f'C {text}'
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+def python_text(text):
+    """ it displays Hello"""
+    text = text.replace('_', ' ')
+    return f'python {text}'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
