556c31d9e9e546b5c653616a4f319de9f35809b5
AMR BY <bouyoussefamr@gmail.com>
2024-04-20 21:07:56 +0000

    mandatory

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -25,6 +25,7 @@ def c_text(text):
     return f'C {text}'
 
 
+@app.route('/python/<text>', strict_slashes=False)
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 def python_text(text):
     """ it displays Hello"""
