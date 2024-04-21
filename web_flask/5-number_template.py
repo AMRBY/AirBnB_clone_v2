6980c39cf6ba4d5d3674103993a135d5ccacba76
AMR BY <bouyoussefamr@gmail.com>
2024-04-21 12:29:11 +0000

    mandatory

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -26,7 +26,7 @@ def c_text(text):
     return f'C {text}'
 
 
-@app.route('/python/', strict_slashes=False)
+@app.route('/python/')
 @app.route('/python/<text>')
 # @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 def python_text(text=None):
