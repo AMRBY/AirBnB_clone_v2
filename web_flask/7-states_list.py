9eefc5c0becc6477725a1b4fcac6e2521ad51e92
AMR BY <bouyoussefamr@gmail.com>
2024-04-22 19:34:58 +0000

    mandatory

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,7 +4,7 @@ from flask import Flask
 from markupsafe import escape
 from flask import render_template
 from models import storage
-from models import state 
+from models.state import State 
 
 app = Flask(__name__)
 
@@ -12,14 +12,14 @@ app = Flask(__name__)
 @app.route("/states_list", strict_slashes=False)
 def states_list():
     """ it displays states"""
-    d = storage.all()
+    d = list(storage.all(State).values())
     return render_template('7-states_list.html', d=d)
 
 
 @app.teardown_appcontext
-def clean_up():
+def clean_up(exception):
     """ it cleans the context """
-    return strorage.close()
+    storage.close()
 
 
 if __name__ == '__main__':
