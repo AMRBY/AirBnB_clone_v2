f1dbbb5a39c7fe94814c6e17f657600c1da9d3a0
AMR BY <bouyoussefamr@gmail.com>
2024-04-22 18:40:22 +0000

    mandatory

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,6 +4,7 @@ from flask import Flask
 from markupsafe import escape
 from flask import render_template
 from models import storage
+from models import state 
 
 app = Flask(__name__)
 
@@ -18,43 +19,7 @@ def states_list():
 @app.teardown_appcontext
 def clean_up():
     """ it cleans the context """
-    return storage.close()
-
-
-@app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
-    """ it displays Hello"""
-    text = text.replace('_', ' ')
-    return f'C {text}'
-
-
-@app.route('/python', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python_text(text=None):
-    """ it displays Hello"""
-    if text is None:
-        return 'Python is cool'
-    else:
-        text = text.replace('_', ' ')
-        return f'Python {text}'
-
-
-@app.route('/number/<int:n>', strict_slashes=False)
-def is_int(n):
-    """ it displays Hello"""
-    return f'{n} is a number'
-
-
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def template_int(n):
-    """ it displays Hello"""
-    return render_template('5-number.html', n=n)
-
-
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def template_odd_even(n):
-    """ it displays Hello"""
-    return render_template('6-number_odd_or_even.html', n=n)
+    return strorage.close()
 
 
 if __name__ == '__main__':
